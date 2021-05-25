6a614c496228200e1097a49471882a55d888161c
Javan Rasokat <mail@javan.de>
2021-05-25 21:27:25 +0200

    test

--- main.sh
+++ main.sh
@@ -20,6 +20,7 @@ echo '[+] Starting mysql...'
 service mysql start
 
 echo '[+] Starting apache'
+sed -i "s/80/$PORT/g" /etc/apache2/sites-enabled/000-default.conf /etc/apache2/ports.conf
 service apache2 start
 
 while true
